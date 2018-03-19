a478e3bad14a15b788b3b208508b1152e2f1aa99
doudoujay <doudoujay@qq.com>
2018-03-19 15:17:26 -0400

    half way hw4

--- ma438-hw3/Tree.py
+++ ma438-hw3/Tree.py
@@ -1,5 +1,16 @@
 class Tree(object):
+    """Tree data structure
+    left: left Tree
+    right: right Tree
+    data: sub_data for each layer
+    """
+
     def __init__(self):
         self.left = None
         self.right = None
         self.data = None
+        self.attr = ""
+        self.leftLabel = ""
+        self.rightLabel = ""
+    def isLeaf(self):
+        return (self.left is None) and (self.right is None)
--- ma438-hw3/decisiontree.py
+++ ma438-hw3/decisiontree.py
@@ -5,16 +5,18 @@ import sys
 import pandas as pd
 import numpy as np
 import scipy.stats
-import Tree
+from Tree import Tree
 
 data = None
 X = None
 trainPercentage = 0.0
+
+
 def main(datasetPath, model, percentage):
     global data
     global X
     global trainPercentage
-    #save train percent
+    # save train percent
     trainPercentage = int(percentage) / 100.0
 
     data = pd.read_csv(datasetPath, sep=',', quotechar='"', header=0)
@@ -23,58 +25,115 @@ def main(datasetPath, model, percentage):
                 'alcohol', 'open', 'goodForGroups']
     data = data[features]
     X = data.as_matrix()
-    X = X[:1000] # debug
-    np.random.shuffle(X) # shuffle the dataset for train and test
+    X = X[:1000]  # debug
+    data = data.sample(frac=1)  # shuffle the dataset for train and test
+    data = data.iloc[:1000,:]
     if model == "vanilla":
         vanilla()
     elif model == "depth":
         depth()
     elif model == "prune":
         prune()
-# the full decision tree
+
+
+def id3(sub_data, label):
+    if check_same_label(sub_data):
+        # If all examples are have same label
+        node = Tree()
+        node.data = sub_data
+        node.label = label
+        return node
+    else:
+        node = Tree()
+        node.attr = best_attribute(sub_data)
+        for idx, label in enumerate(sub_data['node.attr'].value_counts.keys()):
+            if idx == 0 :
+                node.left = id3(sub_data.loc[sub_data[node.attr] == label], label)
+            if idx == 1:
+                node.right = id3(sub_data.loc[sub_data[node.attr] == label], label)
+        return node
+        
+
 def vanilla():
-    #splitting to training and test
-    trainSize = int(trainPercentage * len(X))
-    training, test = X[:trainSize,:], X[trainSize:,:]
-
-    IG(data['open'],data['outdoorSeating'])
-
-
+    """
+    the full decision tree
+    """
+    # splitting to training and test
+    trainSize = int(trainPercentage * len(data))
+    training, test = data.iloc[:trainSize, :], data.iloc[trainSize:, :]
+    print best_attribute(training)
+    print_all_labels(training)
     return 0
-# the decision tree with static depth
+
+
+
 def depth():
+    """
+    the decision tree with static depth
+    """
     depthVal = int(sys.argv[5])
-    #splitting to training, test and validation
-    trainSize = int(trainPercentage * len(X))
-    validSize = int(int(sys.argv[4])/100.0 * len(X))
-    training, test = X[:trainSize,:], X[trainSize:,:]
+    # splitting to training, test and validation
+    trainSize = int(trainPercentage * len(data))
+    validSize = int(int(sys.argv[4])/100.0 * len(data))
+
+    training, test = X[:trainSize, :], X[trainSize:, :]
     valid, test = test[:validSize, :],  test[validSize:, :]
     # print len(training)
     # print len(valid)
     # print len(test)
     return 0
 
-# the decision tree with post-pruning
+
 def prune():
+    """
+    the decision tree with post-pruning
+    """
+
     return 0
 
-# information gainz
-# How much does a feature split decrease the entropy
-def IG(S, A):
-    print S.value_counts()
-    print A.value_counts()
-    temp = 0
-    for v in A.value_counts().values:
-        temp += (v / sum(A.value_counts().values) *
 
-    return H(S) - temp
-
-
-# entropy
 def H(S):
-    p_data= S.value_counts()/len(data)
-    entropy=scipy.stats.entropy(p_data)
+    """
+    entropy
+    """
+
+    p_data = S.value_counts()/len(data)
+    entropy = scipy.stats.entropy(p_data)
     return entropy
+
+
+def IG(data, A):
+    """
+    information gainz
+    """
+ 
+    # print data.loc[data['open'] == True]
+    temp = 0
+    for k in A.value_counts().keys():
+        sub_data = data.loc[A == k]
+        weight = len(sub_data) / float(len(data))
+        temp += weight * H(sub_data['goodForGroups'])
+    # print H(data['goodForGroups'])
+    # print temp
+    return H(data['goodForGroups']) - temp
+
+def check_same_label(data):
+    return len(data['goodForGroups'].value_counts().keys()) == 1
+
+def best_attribute(data):
+    IG_arr = []
+    for col in list(data):
+        if col == 'goodForGroups': continue
+        IG_arr.append(IG(data, data[col]))
+    maxIdx = IG_arr.index(max(IG_arr))
+    return list(data)[maxIdx]
+
+def print_all_labels(data):
+    for col in list(data):
+        if col == 'goodForGroups': continue
+        print data[col].value_counts().keys()
+        
+
 
 if __name__ == "__main__":
     if(len(sys.argv) < 4):
